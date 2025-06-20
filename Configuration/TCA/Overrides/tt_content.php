d4a4a83f2306ef01f8abfbe97162635e9639ddde
achmedsagitov <sagitovakhmed@mail.ru>
2025-06-20 09:28:18 +0200

    [TASK] changing extension key to mautic_form [TER-311] [TER-321]

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -5,21 +5,21 @@
     'tt_content',
     'CType',
     [
-        'label' => 'LLL:EXT:typo3_mautic_form/Resources/Private/Language/locallang_db.xlf:mautic_form.title',
-        'value' => 'typo3_mautic_form',
+        'label' => 'LLL:EXT:mautic_form/Resources/Private/Language/locallang_db.xlf:mautic_form.title',
+        'value' => 'mautic_form',
         'icon' => 'content-form',
         'group' => 'default',
-        'description' => 'LLL:EXT:typo3_mautic_form/Resources/Private/Language/locallang_db.xlf:mautic_form.description',
+        'description' => 'LLL:EXT:mautic_form/Resources/Private/Language/locallang_db.xlf:mautic_form.description',
     ],
     'header',
     'before'
 );
 
-$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['typo3_mautic_form'] = 'content-form';
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['mautic_form'] = 'content-form';
 
 $tempColumns = [
     'mautic_form_id' => [
-        'label' => 'LLL:EXT:typo3_mautic_form/Resources/Private/Language/locallang_db.xlf:mautic_form.id',
+        'label' => 'LLL:EXT:mautic_form/Resources/Private/Language/locallang_db.xlf:mautic_form.id',
         'config' => [
             'type' => 'input',
             'eval' => 'required,int',
@@ -27,8 +27,8 @@ $tempColumns = [
         ],
     ],
     'mautic_server_url' => [
-        'label' => 'LLL:EXT:typo3_mautic_form/Resources/Private/Language/locallang_db.xlf:mautic_form.server_url',
-        'description' => 'LLL:EXT:typo3_mautic_form/Resources/Private/Language/locallang_db.xlf:mautic_form.server_url.description',
+        'label' => 'LLL:EXT:mautic_form/Resources/Private/Language/locallang_db.xlf:mautic_form.server_url',
+        'description' => 'LLL:EXT:mautic_form/Resources/Private/Language/locallang_db.xlf:mautic_form.server_url.description',
         'config' => [
             'type' => 'input',
             'eval' => 'required,trim',
@@ -39,7 +39,7 @@ $tempColumns = [
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumns);
 
 
-$GLOBALS['TCA']['tt_content']['types']['typo3_mautic_form'] = [
+$GLOBALS['TCA']['tt_content']['types']['mautic_form'] = [
     'showitem' => '
         --palette--;;general,
         mautic_server_url,
--- ext_localconf.php
+++ ext_localconf.php
@@ -11,24 +11,24 @@ defined('TYPO3') or die();
 $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
 if ($versionInformation->getMajorVersion() < 12) {
     ExtensionManagementUtility::addPageTSConfig(
-        '@import "EXT:typo3_mautic_form/Configuration/page.tsconfig"',
+        '@import "EXT:mautic_form/Configuration/page.tsconfig"',
     );
 }
 
 
 call_user_func(function()
 {
-    $extensionKey = 'typo3_mautic_form';
+    $extensionKey = 'mautic_form';
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
         $extensionKey,
         'setup',
-        "@import 'EXT:typo3_mautic_form/Configuration/TypoScript/setup.typoscript'"
+        "@import 'EXT:mautic_form/Configuration/TypoScript/setup.typoscript'"
     );
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
         $extensionKey,
         'constants',
-        "@import 'EXT:typo3_mautic_form/Configuration/TypoScript/constants.typoscript'"
+        "@import 'EXT:mautic_form/Configuration/TypoScript/constants.typoscript'"
     );
 });
